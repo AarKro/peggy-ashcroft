9d19a6395b528322f6a8774410d07d5aa5160734
Aaron Kromer <kromer.aaron@gmail.com>
2026-03-06 15:39:23 +0100

    add proper award card text

--- src/pages/awards/Awards.tsx
+++ src/pages/awards/Awards.tsx
@@ -12,12 +12,11 @@ export const Awards = () => {
       <div className="awards">
         {/* <h2 className='awards__subtitle'>11 Awards & 20 Nominations</h2> */}
         <div className='awards__cards'>
-          <AwardCard number='3' title='Awards' />
-          <AwardCard number='3' title='Awards' />
-          <AwardCard number='3' title='Awards' />
-          <AwardCard number='3' title='Awards' />
-          <AwardCard number='3' title='Awards' />
-          <AwardCard number='3' title='Awards' />
+          <AwardCard number='3' title='BAFTAs' />
+          <AwardCard number='1' title='Academy Awards' />
+          <AwardCard number='1' title='Golden Globe' />
+          <AwardCard number='1' title='Laurence Olivier' />
+          <AwardCard number='5' title='Other Awards' />
         </div>
         <p className='awards__text'>
           Check out all her <a target='_blank' href="https://en.wikipedia.org/wiki/Peggy_Ashcroft#Honours,_awards_and_memorials">Accolades</a>
